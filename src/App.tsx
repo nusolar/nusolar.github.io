676b76017d55ec39f9995b6a5dbabb78cc374af9
Bodie <bodie.feinberg@getanswersnow.com>
2026-05-10 14:41:18 -0500

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
-import solarTeam from "./assets/solar_team_2026.jpg";
-import "./App.css";
+import landingVideo from "./assets/landing_video.mp4";
+import sunLogo from "./assets/sun_logo.svg";
 
-function App() {
+export default function App() {
   return (
-    <section id="center">
-      <h1>Solar Car Team Website</h1>
-      <div>
-        <img src={solarTeam} height="500" alt="the solar car team" />
-      </div>
-    </section>
+    <div style={{ position: "relative", width: "100vw", height: "100vh", overflow: "hidden" }}>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+        }}
+      >
+        <source src={landingVideo} type="video/mp4" />
+      </video>
+
+      <img
+        src={sunLogo}
+        alt="NUSolar logo"
+        style={{
+          position: "absolute",
+          top: "24px",
+          left: "24px",
+          width: "64px",
+          height: "64px",
+          zIndex: 10,
+        }}
+      />
+    </div>
   );
 }
-
-export default App;
